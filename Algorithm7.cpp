88067b83022e5fef40dac7db7c9d13cd83eaa0ae
Yavuz Mollahamzaoğlu <yavuz.mersin@hotmail.com>
2021-12-20 19:44:00 +0300

    C Algorithm
    For algorithm practice

---
+++ Algorithm7.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	int x,y;
+	printf("Please enter two different number:");
+	scanf("%d,%d",&x,&y);
+	
+	if(x<y)
+	printf("%d is the greatest number",y);
+	else
+	printf("%d is the greatest number",x);
+	
+	
+	
+	
+	
+	
+	
+
+	return 0;
+}
---
+++ Algorithm8.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int visa,final,ave;
+	printf("Please enter your visa and final score:");
+	scanf("%d,%d",visa,final);
+	ave=(visa+final)/2;
+	if(ave>70)
+	printf("Congratulations you pass!");
+	else
+	printf("You fail");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
